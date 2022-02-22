0fc3c5d260e79f1d471583b2c9e9456df9193f01
Tor-Eirik Bakke Lunde <teblunde@gmail.com>
2022-02-22 23:14:13 +0100

    Disable RX/TX LED

--- software/arduino/The64Genesis/The64Genesis/process_serial.cpp
+++ software/arduino/The64Genesis/The64Genesis/process_serial.cpp
@@ -4,7 +4,11 @@
 
 void init_serial() {
   Serial.begin(115200);
-  
+
+  /* Disable TX and RX LEDs */  
+  pinMode(LED_BUILTIN_TX, INPUT);
+  pinMode(LED_BUILTIN_RX, INPUT);
+
   Serial.print(APP_TITLE);
   Serial.print(" ");
   Serial.println(APP_VERSION);
